915630fa0fe60f8ef397de7704d3eb6e03ef7720
hargun0360 <hargunsingh0720@gmail.com>
2023-02-10 02:52:46 +0530

    Creating Big File Program

--- createBigFile.js
+++ createBigFile.js
@@ -1,1 +1,19 @@
-console.log("hello");
+// node createBigFile.js --dest = f1.
+
+let minimist = require("minimist");
+let fs  = require("fs");
+
+let args = minimist(process.argv);
+
+let arr = [];
+for(let i=0 ; i<50000000 ; i++){
+    arr.push(i);
+}
+
+let dtext = arr.join("\n");
+
+
+// Write create new file or rewrite the existing file from scratch
+fs.writeFileSync(args.dest , dtext , "utf-8");
+// append add the data after the content in existing file.
+fs.appendFileSync(args.dest , dtext , "utf-8")
